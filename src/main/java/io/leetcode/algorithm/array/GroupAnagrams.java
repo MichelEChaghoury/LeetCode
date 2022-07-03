d207c6eaa197e58cbe1fe6b02f8bea16b37699ed
Michel Chaghoury <michel.e.chaghoury@gmail.com>
2022-07-03 14:02:42 +0300

    Add Group Anagrams Problem with Solution.

---
+++ src/main/java/io/leetcode/algorithm/array/GroupAnagrams.java
@@ -0,0 +1,71 @@
+package io.leetcode.algorithm.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    // LeetCode 49 | Level: Medium
+    // https://leetcode.com/problems/group-anagrams/
+
+    // Given an array of strings strs, group the anagrams together. You can return
+    // the answer in any order.
+
+    // An Anagram is a word or phrase formed by rearranging the letters of a
+    // different word or phrase, typically using all the original letters exactly
+    // once.
+
+    // Example 1:
+    // Input: strs = ["eat","tea","tan","ate","nat","bat"]
+    // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+    // Example 2:
+    // Input: strs = [""]
+    // Output: [[""]]
+
+    // Example 3:
+    // Input: strs = ["a"]
+    // Output: [["a"]]
+
+    // Constraints:
+
+    // 1 <= strs.length <= 104
+    // 0 <= strs[i].length <= 100
+    // strs[i] consists of lowercase English letters.
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        List<List<String>> results = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        if (strs.length == 1) {
+            List<String> value = List.of(strs);
+            results.add(value);
+
+            return results;
+        }
+
+        for (int i = 0; i < strs.length; i++) {
+            String str = strs[i];
+            char[] chars = str.toCharArray();
+
+            Arrays.sort(chars);
+            String key = Arrays.toString(chars);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
+        }
+
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            List<String> values = entry.getValue();
+            results.add(values);
+        }
+
+        return results;
+    }
+}
--- src/main/java/io/leetcode/algorithm/array/ValidAnagram.java
+++ src/main/java/io/leetcode/algorithm/array/ValidAnagram.java
@@ -15,11 +15,10 @@ public class ValidAnagram {
     // once.
 
     // Example 1:
-
     // Input: s = "anagram", t = "nagaram"
     // Output: true
-    // Example 2:
 
+    // Example 2:
     // Input: s = "rat", t = "car"
     // Output: false
 
